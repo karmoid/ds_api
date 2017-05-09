9e2f3f32f90d8d793748a16661560eff2f0a7aa2
Marc Chauffour <karmoc@gmail.com>
2017-05-09 23:13:06 +0200

    Modification cosmétique sur Admin Device et Place

--- app/admin/place.rb
+++ app/admin/place.rb
@@ -11,4 +11,52 @@ permit_params do
   permitted
 end
 
+# show do
+#   panel "Détail de l'emplacement" do
+#     attributes_table_for place do
+#       row :name
+#       row :geoloc
+#       row :updated_at
+#       row :created_at
+#     end
+#   end
+#   attributes_table_for place do
+#     row "Places" do |pl|
+#       pl.devices.each.map do |device|
+#         link_to(device.name, admin_device_path(device))
+#       end.join(', ').html_safe
+#     end
+#   end
+#   panel "Equipements associées" do
+#     table_for place.devices do |pl|
+#       column :name
+#       column :action do
+#         link_to :view, admin_device_path(device)
+#       end
+#       column :ip
+#       column :uuid
+#     end
+#   end
+# end
+
+show do
+  panel "Equipements" do
+    table_for place.devices do
+      column("Name") {|device| link_to(device.name, admin_device_path(device))}
+      column :id
+      column :ip
+      column :uuid
+      column ("Edit") {|device| link_to("Edit", edit_admin_device_path(device))}
+    end
+  end
+end
+sidebar "Emplacement", only: [:show] do
+  attributes_table_for place do
+    row :name
+    row :geoloc
+    row :updated_at
+    row :created_at
+  end
+end
+
 end
